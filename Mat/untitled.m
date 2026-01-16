5780809053699549c15299bc31da1bbec0c06a3a
SSSCUAT <reynosodiego264@gmail.com>
2026-01-15 19:51:15 -0800

    Induced Drag calculation

---
+++ Mat/untitled.m
@@ -0,0 +1,14 @@
+%% 154A Project 
+%known values 
+
+% Aerodynamic Variable Definitions
+CL = 1.5;    % 3D wing lift coefficient
+AR = 7.4;    % Wing aspect ratio (Example value for Cessna 182)
+CD_p = 0.027; %parasite Drag 
+
+% Induced Drag Calculation
+% Formula from image: CDi = CL^2 / (pi * AR)
+CDi = (CL^2) / (pi * AR);
+
+% Display Result
+fprintf('The Induced Drag Coefficient (CDi) is: %.4f\n', CDi);
