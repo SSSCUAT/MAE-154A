dffbffde87e31bea4fa6c2378b36c208406ee34f
SSSCUAT <reynosodiego264@gmail.com>
2026-01-15 20:15:19 -0800

    added efficiency factor e

--- Mat/Main.m
+++ Mat/Main.m
@@ -5,6 +5,7 @@
 CL = 1.5;    % 3D wing lift coefficient
 AR = 7.4;    % Wing aspect ratio (Example value for Cessna 182)
 CD_p = 0.027; %parasite Drag 
+e = 0.80;            % Oswald's efficiency factor
 
 % Induced Drag Calculation
 % Formula from image: CDi = CL^2 / (pi * AR)
